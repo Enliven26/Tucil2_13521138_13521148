3c144b4939305882539f0ad56bb18930dcb89bd8
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-02-27 19:08:52 +0700

    main

--- src/main.py
+++ src/main.py
@@ -1,25 +1,49 @@
 import Points as p
 import IO
 import ClosestPair as cp
+import time
 
-for i in range(100):
-       p1 = p.generateRandomPoints(100, 1, -1e6, 1e6)
+def main():
 
-       p.sort(p1)
+       stop = False
 
-       res1 = cp.findNearestPair(p1, 1)
-       print(p.counter.count)
-
-       res2 = cp.findNearestPair(p1)
-       print(p.counter.count)
-       
-       if (res1[0] != res2[0]): 
-              print("BF result: ", res1)
-              print("DNC result: ", res2)
+       IO.art()
+       IO.welcome()
+       while(not(stop)):
               
-              print("ERROR")
-              print(p1)
-              exit()
+              choice = IO.getChoices(['Start', 'Exit'], "Action")
 
+              if (choice == 1):
 
-print("CONGRATS!")
+                     n = int(input("Points amount: "), end='')
+                     dimension = int(input("Input dimension: "), end='')
+
+                     choice = IO.getChoices(['Console', 'File', 'Random'])
+
+                     # if (choice == 1):
+
+                     # elif (choice == 2):
+
+                     if (choice == 3):      
+                            p1 = p.generateRandomPoints(n, dimension, -1e6, 1e6)
+
+                     p.sort(p1)
+
+                     start = time.time()
+                     BFresult = cp.findNearestPair(p1, 1)
+                     end = time.time()
+                     print('Brute Force getDistance() calls count :', p.counter.count)
+                     print('Brute Force execution time:', (end - start) * (10 ** 3), 'ms')
+
+                     print('\n')
+
+                     start = time.time()
+                     DNCresult = cp.findNearestPair(p1)
+                     end = time.time()
+
+                     print('Divide and Conquer getDistance() calls count :', p.counter.count)
+                     print('Divide and Conquer execution time:', (end - start) * (10 ** 3), 'ms')
+                     print('\n')
+
+       
+       exit()
